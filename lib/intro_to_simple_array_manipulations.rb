9c37abe3afe3d0ed6119ae9c6fa812cd5b897a51
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-06-27 13:04:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,16 +1,16 @@
-def using_concat
+def using_concat(array1, array2)
 end 
 
 
-def using_insert
+def using_insert(array, element)
 end
 
 
-def using_uniq
+def using_uniq(array)
 end
 
 
-def using_flatten
+def using_flatten(array)
 end
 
 
